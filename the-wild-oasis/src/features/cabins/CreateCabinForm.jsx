66a3292a405790c42d49eb63fb91c69ce470378f
sephine.k31@gmail.com <sephine.k31@gmail.com>
2024-07-22 23:01:40 -0700

    advanced react patterns

--- the-wild-oasis/src/features/cabins/CreateCabinForm.jsx
+++ the-wild-oasis/src/features/cabins/CreateCabinForm.jsx
@@ -10,7 +10,7 @@ import FormRow from '../../ui/FormRow';
 import UseCreateCabin from './useCreateCabin';
 import UseEditCabin from './useEditCabin';
 
-function CreateCabinForm({ cabinToEdit = {} }) {
+function CreateCabinForm({ cabinToEdit = {}, onCloseModal }) {
   const { isCreating, createCabin } = UseCreateCabin();
   const { isEditing, editCabin } = UseEditCabin();
   const isWorking = isCreating || isEditing;
@@ -30,14 +30,20 @@ function CreateCabinForm({ cabinToEdit = {} }) {
       editCabin(
         { newCabinData: { ...data, image }, id: editId },
         {
-          onSuccess: () => reset(),
+          onSuccess: () => {
+            reset();
+            onCloseModal?.();
+          },
         }
       );
     else
       createCabin(
         { ...data, image: image },
         {
-          onSuccess: () => reset(),
+          onSuccess: () => {
+            reset();
+            onCloseModal?.();
+          },
         }
       );
   }
@@ -47,7 +53,10 @@ function CreateCabinForm({ cabinToEdit = {} }) {
   }
 
   return (
-    <Form onSubmit={handleSubmit(onSubmit, onError)}>
+    <Form
+      onSubmit={handleSubmit(onSubmit, onError)}
+      type={onCloseModal ? 'modal' : 'regular'}
+    >
       <FormRow label='Cabin name' error={errors?.name?.message}>
         <Input
           type='text'
@@ -131,7 +140,11 @@ function CreateCabinForm({ cabinToEdit = {} }) {
 
       <FormRow>
         {/* type is an HTML attribute! */}
-        <Button variation='secondary' type='reset'>
+        <Button
+          variation='secondary'
+          type='reset'
+          onClick={() => onCloseModal?.()}
+        >
           Cancel
         </Button>
         <Button disabled={isWorking}>
