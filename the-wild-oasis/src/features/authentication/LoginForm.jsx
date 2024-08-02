3e5fb50e22af4c4d007a24f2fc3f2cced6f80a29
sephine.k31@gmail.com <sephine.k31@gmail.com>
2024-08-01 17:48:41 -0700

    .

--- the-wild-oasis/src/features/authentication/LoginForm.jsx
+++ the-wild-oasis/src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from './useLogin';
 import SpinnerMini from '../../ui/SpinnerMini';
 
 function LoginForm() {
-  const [email, setEmail] = useState('sephine@example.com');
-  const [password, setPassword] = useState('123456789');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const { login, isPending } = useLogin();
 
   function handleSubmit(e) {
